4f5e723f9e6d43a2b17444920156532d6bbcde14
akrivda <akrivda@yandex.ru>
2020-02-01 05:52:10 +0300

    beta 1.6

---
+++ sql/V4.0.02__hidden_attributes.sql
@@ -0,0 +1,17 @@
+alter table metatype
+    add hidden boolean default false not null;
+
+alter table ability_pack
+    add hidden boolean default false not null;
+
+alter table archetype
+    add hidden boolean default false not null;
+
+alter table archetype_feature
+    add hidden boolean default false not null;
+
+alter table aspect
+    add hidden boolean default false not null;
+
+alter table character_feature
+    add hidden boolean default false not null;
