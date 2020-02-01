6dc57253c04681c6bb740d9ee58c80ac287d6dcd
ranvas <sluchajWE@yandex.ru>
2020-02-01 18:48:49 +0300

    + hangfire_jobs

---
+++ sql/V5.0.01.sql
@@ -0,0 +1,25 @@
+create table if not exists hangfire_job_type
+(
+    id   serial  not null,
+	name varchar(255) not null
+);
+
+alter table hangfire_job_type
+    owner to billing;
+
+
+create table if not exists hangfire_job
+(
+    id   serial  not null,
+	start_time datetime not null,
+    end_time datetime not null,
+	cron varchar(255) not null,
+	job_name varchar(255) not null,
+	constraint job_type references hangfire_job_type on delete cascade,
+	hangfire_startid varchar(255) null,
+	hangfire_reccurentid varchar(255) null,
+	hangfire_endid varchar(255) null
+);
+
+alter table hangfire_job
+    owner to billing;
