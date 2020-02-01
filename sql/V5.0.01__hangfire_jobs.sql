88bc18d9f7dec65d7446fef9f17c1ff7922e7d44
ranvas <sluchajWE@yandex.ru>
2020-02-01 22:58:49 +0300

    fixes

--- sql/V5.0.01__hangfire_jobs.sql
+++ sql/V5.0.01__hangfire_jobs.sql
@@ -1,22 +1,23 @@
 create table if not exists hangfire_job_type
 (
-    id   serial  not null,
-	
+    id   serial  not null
+		constraint hangfire_job_type_pk primary key,
 	name varchar(255)  not null
 );
 
 alter table hangfire_job_type
     owner to billing;
 
-
 create table if not exists hangfire_job
 (
-    id   serial  not null,
+    id   serial  not null
+		constraint hangfire_job_pk primary key,
 	start_time timestamp with time zone not null,
     end_time timestamp with time zone not null,
 	cron varchar(255) not null,
 	job_name varchar(255) not null,
-	constraint job_type references hangfire_job_type on delete cascade,
+	job_type integer not null 
+		constraint job_type_fk references hangfire_job_type(id) on delete cascade,
 	hangfire_startid varchar(255) null,
 	hangfire_reccurentid varchar(255) null,
 	hangfire_endid varchar(255) null
