ac7476a97bb85ead93c3012d949d5c7c9565a0a8
ranvas <sluchajWE@yandex.ru>
2020-02-16 18:50:08 +0300

    fix

--- sql/V5.0.03__lifestyle.sql
+++ sql/V5.0.03__lifestyle.sql
@@ -1,4 +1,4 @@
-alter table sin_details drop CONSTRAINT sin_details_lifestyle_fk
-alter table transfer drop constraint transfer_lifestylefrom_fk
-alter table transfer drop constraint transfer_lifestyleto_fk
+alter table sin_details drop CONSTRAINT if exists sin_details_lifestyle_fk
+alter table transfer drop constraint if exists transfer_lifestylefrom_fk
+alter table transfer drop constraint if exists transfer_lifestyleto_fk
 drop table if exists lifestyle
