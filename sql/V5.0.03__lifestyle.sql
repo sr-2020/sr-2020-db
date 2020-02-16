56d4cee2ebc601e6f3659ebc8c7c3292b23141f4
ranvas <sluchajWE@yandex.ru>
2020-02-16 18:56:34 +0300

    + semicolumn

--- sql/V5.0.03__lifestyle.sql
+++ sql/V5.0.03__lifestyle.sql
@@ -1,4 +1,4 @@
-alter table sin_details drop CONSTRAINT if exists sin_details_lifestyle_fk
-alter table transfer drop constraint if exists transfer_lifestylefrom_fk
-alter table transfer drop constraint if exists transfer_lifestyleto_fk
-drop table if exists lifestyle
+alter table sin_details drop constraint if exists sin_details_lifestyle_fk;
+alter table transfer drop constraint if exists transfer_lifestylefrom_fk;
+alter table transfer drop constraint if exists transfer_lifestyleto_fk;
+drop table if exists lifestyle;
