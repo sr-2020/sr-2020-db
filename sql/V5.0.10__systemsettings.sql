235edbad95f6f1476db9572404f31bedf1283c28
ranvas <sluchajWE@yandex.ru>
2020-02-24 20:02:10 +0300

    fix

--- sql/V5.0.10__systemsettings.sql
+++ sql/V5.0.10__systemsettings.sql
@@ -1,7 +1,7 @@
 create table if not exists system_settings
 (
     id serial not null
-		constraint scoring_factor_calculate_pk primary key,
+		constraint system_settings_calculate_pk primary key,
 	key varchar(255) not null,
 	value varchar(255) not null
 );
