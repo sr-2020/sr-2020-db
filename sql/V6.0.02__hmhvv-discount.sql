10f46757cfecf22b429f191e616bf614a83259eb
ranvas <sluchajWE@yandex.ru>
2020-03-10 02:08:57 +0300

    + Кривда редиска

---
+++ sql/V6.0.02__hmhvv-discount.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.aspect_discount (id, metatype, aspect, kind)
+VALUES (36, 7, 15, 'small');
+INSERT INTO public.aspect_discount (id, metatype, aspect, kind)
+VALUES (37, 7, 1, 'small');
+INSERT INTO public.aspect_discount (id, metatype, aspect, kind)
+VALUES (38, 7, 2, 'small');
+INSERT INTO public.aspect_discount (id, metatype, aspect, kind)
+VALUES (39, 7, 3, 'small');
+INSERT INTO public.aspect_discount (id, metatype, aspect, kind)
+VALUES (40, 7, 10, 'huge');
+INSERT INTO public.aspect_discount (id, metatype, aspect, kind)
+VALUES (41, 7, 11, 'small');
+INSERT INTO public.aspect_discount (id, metatype, aspect, kind)
+VALUES (42, 7, 12, 'small');
+INSERT INTO public.aspect_discount (id, metatype, aspect, kind)
+VALUES (43, 7, 7, 'small');
+INSERT INTO public.aspect_discount (id, metatype, aspect, kind)
+VALUES (44, 7, 9, 'small');
