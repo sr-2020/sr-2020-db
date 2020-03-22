9cfc5bc671f991777036acd66df8e991f4f0aa76
ranvas <sluchajWE@yandex.ru>
2020-03-22 11:57:47 +0300

    testshop

---
+++ sql/V7.0.14__testshop.sql
@@ -0,0 +1,10 @@
+alter table shop_wallet drop column foreign;
+do $$
+declare myid integer;
+begin
+	select id from wallet where wallet_type = 3 into myid;
+
+	if not exists (select * from shop_wallet where name = 'testshop') then
+		insert into shop_wallet(wallet, name, lifestyle) values(myid, 'testshop', 1);
+	end if;
+end
