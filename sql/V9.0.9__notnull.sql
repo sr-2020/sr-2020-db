1fd74b19ddd3790ce3e43ffedc09dfef1e14861f
ranvas <sluchajWE@yandex.ru>
2020-06-27 19:13:28 +0300

    fix

--- sql/V9.0.9__notnull.sql
+++ sql/V9.0.9__notnull.sql
@@ -1,3 +1,3 @@
 
-alter table shop wallet  drop column if exists owner;
+alter table shop_wallet  drop column if exists owner;
 alter table shop_wallet add column if not exists owner integer not null default 0;
