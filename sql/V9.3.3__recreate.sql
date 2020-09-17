eec911a39e9a416f257fef6d6557b8ed9100d43c
ranvas <sluchajWE@yandex.ru>
2020-09-17 14:24:41 +0300

    recreate

---
+++ sql/V9.3.3__recreate.sql
@@ -0,0 +1,67 @@
+drop table if exists scoring_factor_calculate;
+drop table if exists scoring_category_calculate;
+drop table if exists current_category;
+drop table if exists current_factor;
+drop table if exists event_lifestyle;
+drop table if exists scoring_factor;
+drop table if exists scoring_category;
+create table if not exists scoring_category
+(
+    id  serial  not null
+		constraint scoring_category_pk primary key,
+	name text not null,
+	cat_type integer not null,
+	weight numeric not null
+);
+alter table scoring_category
+    owner to backend;
+	
+create table if not exists current_category
+(
+    id  serial  not null
+		constraint current_category_pk primary key,
+	scoring_category integer  not null
+		constraint current_category_scoring_category_fk references scoring_category(id) on delete cascade,
+	scoring integer not null
+		constraint current_category_scoring_fk references scoring(id) on delete cascade,
+	current_value numeric not null
+);
+alter table current_category
+    owner to backend;
+	
+create table if not exists scoring_factor
+(
+    id  serial  not null
+		constraint scoring_factor_pk primary key,
+	name text not null,
+	scoring_category integer  not null
+		constraint scoring_factor_scoring_category_fk references scoring_category(id) on delete cascade,
+	code varchar(100) not null
+);
+alter table scoring_factor
+    owner to backend;
+	
+create table if not exists current_factor
+(
+    id  serial  not null
+		constraint current_factor_pk primary key,
+	scoring_factor integer  not null
+		constraint current_factor_scoring_factor_fk references scoring_factor(id) on delete cascade,
+	scoring integer not null
+		constraint current_factor_scoring_fk references scoring(id) on delete cascade,
+	current_value numeric not null
+);
+alter table current_factor
+    owner to backend;
+	
+create table if not exists event_lifestyle
+(
+    id  serial  not null
+		constraint event_lifestyle_pk primary key,
+	scoring_factor integer  not null
+		constraint event_lifestyle_scoring_factor_fk references scoring_factor(id) on delete cascade,
+	event_number integer not null,
+	value numeric not null
+);
+alter table event_lifestyle
+    owner to backend;	
