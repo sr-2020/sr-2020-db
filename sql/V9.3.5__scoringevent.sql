5668d36e2fe2715fd25a506e264f42a4796cfcc4
ranvas <sluchajWE@yandex.ru>
2020-09-18 14:12:33 +0300

    time fix

--- sql/V9.3.5__scoringevent.sql
+++ sql/V9.3.5__scoringevent.sql
@@ -6,8 +6,8 @@ create table if not exists scoring_event
 		constraint scoring_event_scoring_fk references scoring(id) on delete cascade,
 	scoring_factor integer  not null
 		constraint scoring_event_scoring_factor_fk references scoring_factor(id) on delete cascade,
-	start_time datetime not null,
-	finish_time datetime not null
+	start_time timestamp with time zone  not null,
+	finish_time timestamp with time zone not null
 );
 alter table scoring_event
     owner to backend;
