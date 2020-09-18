12fb4f7794b1aefcb5bd06ad79fcba590335c70a
ranvas <sluchajWE@yandex.ru>
2020-09-18 15:03:29 +0300

    fix script

--- sql/V9.3.5__testscoring.sql
+++ sql/V9.3.5__testscoring.sql
@@ -5,13 +5,13 @@ begin
 	if not exists (select * from scoring_category where name = 'test') then
 		insert into scoring_category (name, cat_type, weight) values('test', 0, 0);
 	end if;
-	select testcatid from scoring_category where name = 'test';
+	select id into testcatid from scoring_category where name = 'test';
 	
 	if not exists (select * from scoring_factor where code = 'test') then
 		insert into scoring_factor (name, scoring_category, code) values('test', testcatid, 'test');
 	end if;
 	
-	select testfactorid from scoring_factor where code = 'test';
+	select id into testfactorid from scoring_factor where code = 'test';
 	
 	if exists (select * from system_settings where key = 'factor_test') then
 		update system_settings set value = testfactorid where key = 'factor_test';
