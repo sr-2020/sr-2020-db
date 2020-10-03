0b1e4b2f4b9d0ff7c347cd2bf358976612263892
ranvas <sluchajWE@yandex.ru>
2020-10-03 20:14:22 +0300

    script fix

--- sql/V9.5.3__insurance.sql
+++ sql/V9.5.3__insurance.sql
@@ -4,7 +4,7 @@ declare insuranceid integer;
 begin
 	if not exists (select * from product_type where name = 'Страховка') then
 		insert into product_type (name, discount_type, external_id) values('Страховка', 1, 0);
-		
+	end if;
 	select id into insuranceid from product_type where name = 'Страховка';
 	
 	if not exists (select * from system_settings where key = 'insuranceid') then
