93454506057e024de38a7e033f6c2d8935edc29a
akrivda <akrivda@yandex.ru>
2020-10-25 18:03:20 +0300

    fixed permissions, removed race

---
+++ sql/V9.8.1__fixed_permissions.sql
@@ -0,0 +1,9 @@
+alter table metatype_feature  owner to backend;
+alter table general_feature  owner to backend;
+alter table build_gf  owner to backend;
+alter table build_mf  owner to backend;
+alter table feature  owner to backend;
+alter table implant  owner to backend;
+alter table pill  owner to backend;
+alter table drone  owner to backend;
+alter table reagent  owner to backend;
