b6faebce6d3ebac7339949a4180d23086ac9f8bc
ranvas <sluchajWE@yandex.ru>
2020-10-25 05:18:15 +0300

    script fix

--- sql/V9.5.7__raceconstraint.sql
+++ sql/V9.5.7__raceconstraint.sql
@@ -1,3 +1,3 @@
 alter table sin drop column metatype;
 alter table sin add column if not exists metatype int null 
-	constraint if not exists sin_metatype_fk references metatype(id) on delete cascade;
+	constraint sin_metatype_fk references metatype(id) on delete cascade;
