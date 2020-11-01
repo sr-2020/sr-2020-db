4e19660db72ba30286e5ea8a56cc03f39571060f
ranvas <sluchajWE@yandex.ru>
2020-11-02 02:22:33 +0300

    script fix

--- sql/V9.9.1__abilityused.sql
+++ sql/V9.9.1__abilityused.sql
@@ -2,8 +2,8 @@ create table if not exists billing_ability_used
 (
     id serial not null
 		constraint billing_ability_used_pk primary key,
-	public string message,
-	public string key
+	 text message,
+	 text key
 	
 );
 alter table billing_ability_used
