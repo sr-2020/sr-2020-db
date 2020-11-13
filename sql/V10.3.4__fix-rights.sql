f146a60eb2e9b78d98c61a89c95be058300c4192
akrivda <akrivda@yandex.ru>
2020-11-13 04:01:18 +0300

    r1.21.5

---
+++ sql/V10.3.4__fix-rights.sql
@@ -0,0 +1,18 @@
+alter table cyberdeck
+    owner to backend;
+alter table ic_instance
+    owner to backend;
+alter table node_link
+    owner to backend;
+alter table subnode
+    owner to backend;
+alter table software
+    owner to backend;
+alter table node_icon
+    owner to backend;
+alter table icon_file
+    owner to backend;
+alter table icon_api
+    owner to backend;
+alter table icon_portal
+    owner to backend;
