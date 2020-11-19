10eff23f0ac021b554e0d45093bddf38ec75b9da
vivchenkov <vivchenkov@mzt.ru>
2020-11-19 16:13:24 +0300

    corporations

---
+++ sql/V10.6.2__corporations.sql
@@ -0,0 +1,35 @@
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Ares Macrotechnolgy', 0);
+
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Aztechnology (ORO)', 0);
+
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Saeder-Krupp', 0);
+
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Spinrad Global (JRJ INT)', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'NeoNet1 (TransLatviaSeledir)', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'EVO', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Horizon', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Wuxing', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Россия', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Посольство Японии (Sony)', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Цюрих-Орбиталл', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Renraku', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Mutsuhama', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'Shiavase', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'ИИ', 0);
+insert into wallet(id, balance, wallet_type) values(nextval('wallet_id_seq'), 0, 2);
+insert into corporation_wallet(wallet, name, owner) values(currval('wallet_id_seq'), 'JoyCorp', 0);
