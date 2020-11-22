ebe99d6994072b3f7f99b5cd0a5514ce66957797
vivchenkov <vivchenkov@mzt.ru>
2020-11-22 18:52:33 +0300

    + sin

---
+++ sql/V10.6.3__sinfields.sql
@@ -0,0 +1,8 @@
+alter table sin add mt_old integer null;
+alter table sin add nation text null;
+alter table sin add nation_display text null;
+alter table sin add citizenship text null;
+alter table sin add citizen_state text null;
+alter table sin add viza text null;
+alter table sin add mortgagee text null;
+
