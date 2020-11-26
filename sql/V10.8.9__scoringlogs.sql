311859285895c09caac80cfe23e1273e6b7413e8
vivchenkov <vivchenkov@mzt.ru>
2020-11-26 21:19:25 +0300

    scoring logs

---
+++ sql/V10.8.9__scoringlogs.sql
@@ -0,0 +1,4 @@
+alter table scoring_event add old_factor_value numeric null;
+alter table scoring_event add new_factor_value numeric null;
+alter table scoring_event add old_cat_value numeric null;
+alter table scoring_event add new_cat_value numeric null;
