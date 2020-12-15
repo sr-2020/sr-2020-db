deb8883315e94e9bc7764e247d34f7a72e522522
vivchenkov <vivchenkov@mzt.ru>
2020-12-15 20:13:07 +0300

    if not exists

--- sql/V10.10.1__specialisationchange.sql
+++ sql/V10.10.1__specialisationchange.sql
@@ -1,3 +1,3 @@
 delete from specialisation;
 alter table specialisation drop CONSTRAINT IF EXISTS specialisation_product_type;
-alter table specialisation add nomenklatura integer CONSTRAINT specialisation_nomenklatura not null;
+alter table specialisation add if not exists nomenklatura integer CONSTRAINT specialisation_nomenklatura not null;
