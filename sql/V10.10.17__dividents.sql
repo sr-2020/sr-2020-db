15c4d93ebab03ae0c98f08e1bf803534c5652a87
vivchenkov <vivchenkov@mzt.ru>
2021-04-12 16:03:19 +0300

    + dividents

---
+++ sql/V10.10.17__dividents.sql
@@ -0,0 +1,3 @@
+insert into system_settings(key, value) values('dividents1_k', '100');
+insert into system_settings(key, value) values('dividents2_k', '200');
+insert into system_settings(key, value) values('dividents3_k', '300');
