29291ad19ca0ff59eb1acff4c8c105d5f94f7aea
vivchenkov <vivchenkov@mzt.ru>
2021-05-06 19:06:36 +0300

    add passport

---
+++ sql/V11.1.1__addpassport.sql
@@ -0,0 +1,28 @@
+create table if not exists passport
+(
+    id  serial  not null
+	constraint passport_pk primary key,
+	sin_text       varchar(255) constraint uk_rbsiv90dyf0wibv0nja5q07t5 unique,
+	person_name    varchar(255),
+    metatype       integer constraint passport_metatype_fk references metatype on delete cascade,
+    citizenship    varchar(255),
+    mortgagee     varchar(255),
+    viza           varchar(255)
+);
+alter table passport
+    owner to backend;
+
+
+alter table sin add COLUMN if not exist passport integer NULL 
+	CONSTRAINT sin_passport_fk references passport(id);
+	
+alter table sin drop constraint IF EXISTS sin_metatype_fk;
+
+alter table sin add COLUMN if not exist metatype_last integer NULL 
+	CONSTRAINT sin_metatype_fk references metatype(id);
+	
+alter table sin add COLUMN if not exist citizenship_last integer NULL;
+	
+alter table sin add COLUMN if not exist mortgagee_last integer NULL;
+
+alter table sin add COLUMN if not exist viza_last integer NULL;
