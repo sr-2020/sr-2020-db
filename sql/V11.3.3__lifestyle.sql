1f17bfcdc7da8909f921395b1e56263387317c18
vivchenkov <vivchenkov@mzt.ru>
2021-05-17 19:26:02 +0300

    + update ls_dto

---
+++ sql/V11.3.3__lifestyle.sql
@@ -0,0 +1,9 @@
+update system_settings set value = '{
+  "sumAll": 0.0,
+  "forecastSumAll": 0.0,
+  "min": null,
+  "forecastMin": null,
+  "max": null,
+  "forecastMax": null,
+  "count": 0
+}' where key = 'ls_dto'
