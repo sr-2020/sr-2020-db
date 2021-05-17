79ac095d86806642f127154fa44d3aaf3b7db4bf
vivchenkov <vivchenkov@mzt.ru>
2021-05-17 19:52:32 +0300

    fix

--- sql/V11.3.4__lifestyle.sql
+++ sql/V11.3.4__lifestyle.sql
@@ -1,21 +1,10 @@
-declare @dgcode varchar(10)
-
-declare ins_cursor CURSOR for
-select dg_code
-	from dogovor where dg_filialkey = 37106 and dg_turdate > '20210406'
-
-OPEN ins_cursor;
-fetch NEXT from ins_cursor into @dgcode;
-WHILE @@FETCH_STATUS = 0  
-BEGIN 
-insert into history (HI_DGCOD, HI_WHO, HI_TEXT, HI_MOD) values(@dgcode, 'Sokolova T.', 'Уважаемые коллеги! 
-Информируем, что с 1.04 все Ваши заявки курирует офис в г. Москва. 
-Необходимо заключить договор с  «Музенидис Москва»
-Тел: 88002008822
-Почта: tour@mzt.ru', 'MTC')
-fetch NEXT from ins_cursor into @dgcode;
-END
-
-
-CLOSE ins_cursor;
-DEALLOCATE ins_cursor;
+update system_settings set value = '{
+  "bronze": 0.0,
+  "forecastBronze": 0.0,
+  "silver": 0.0,
+  "forecastSilver": 0.0,
+  "gold": 0.0,
+  "forecastGold": 0.0,
+  "platinum": 0.0,
+  "forecastPlatinum": 0.0
+}' where key = 'ls_dto'
