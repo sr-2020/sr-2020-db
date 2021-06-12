34cbaf7b4262706ddb8e1af2f82c174c2b995449
vivchenkov <vivchenkov@mzt.ru>
2021-06-12 11:04:24 +0300

    add corpspec

---
+++ sql/V11.7.1__addcorpspec.sql
@@ -0,0 +1,12 @@
+create table if not exists corporation_specialisation
+(
+    id  serial  not null
+		constraint corporation_specialisation_pk primary key,
+    corporation  integer corporation_specialisation_corporation_fk references corporation_wallet on delete cascade,
+	specialisation integer corporation_specialisation_specialisation_fk references specialisation on delete cascade,
+	ratio numeric not null
+);
+alter table corporation_specialisation
+    owner to backend;
+
+
