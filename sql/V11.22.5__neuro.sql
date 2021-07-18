1c45bb383687b78633c665b3852271ed61c0f504
akrivda <akrivda@yandex.ru>
2021-07-18 07:23:47 +0300

    release 1.63.0

---
+++ sql/V11.22.5__neuro.sql
@@ -0,0 +1,22 @@
+alter table if exists host add if not exists nuero_current  int default 0 not null;
+alter table if exists host add if not exists neuro_gain     int default 0 not null;
+alter table if exists host add if not exists neuro_limit    int default 0 not null;
+alter table if exists host add if not exists neuro_gain_trail varchar(255);
+
+create table if not exists neuro_settings
+  (
+    id             serial        not null constraint neuro_settings_pk primary key,
+    gain_speed     int default 0 not null,
+    spend_research int default 0 not null,
+    transfer_amt   int           not null
+  );
+
+alter table neuro_settings owner to backend;
+
+create table if not exists current_cycle
+  (
+    id      serial        not null,
+    current int default 0 not null
+  );
+
+alter table if exists current_cycle owner to backend;
