c4afb90696805a42ab3f12648f1de835d2b58a80
vivchenkov <vivchenkov@mzt.ru>
2021-07-26 13:29:40 +0300

    default fix

--- sql/V11.29.1__priceforcomission.sql
+++ sql/V11.29.1__priceforcomission.sql
@@ -1,2 +1,2 @@
-alter table price add column if not exists price numeric not null;
-alter table renta add column if not exists shopprice numeric not null;
+alter table price add column if not exists shopprice numeric not null default 0;
+alter table renta add column if not exists shopprice numeric not null default 0;
